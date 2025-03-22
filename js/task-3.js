a04d7ec258737722458e810a1bb5f64db88413a0
Ekaterina <katedubinets22@gmail.com>
2025-03-23 00:33:35 +0200

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,9 @@
 const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output")
 
-nameInput.addEventListener("blur", handleBlur);
+nameInput.addEventListener("input", handleInput);
 
-function handleBlur(event){
+function handleInput(event){
     const name = event.target.value.trim();
     nameOutput.textContent = name ? name : "Annoymous";
     console.log(event);
